c2f51f865f32bbe4d02c6a06a3f03ea1a8345551
Yulu Pan <yulupan@oprime.ib>
2026-04-07 16:49:59 -0400

    Add hockey and soccer forecasting questions (3/5 min videos)

--- rebuild_questions.sh
+++ rebuild_questions.sh
@@ -9,6 +9,8 @@ import json, os
 
 configs = [
     ("data/basketball_questions_3.json", "data/questions.js"),
+    ("data/hockey_questions.json", "data/hockey_questions.js"),
+    ("data/soccer_questions.json", "data/soccer_questions.js"),
 ]
 
 for src, dst in configs:
